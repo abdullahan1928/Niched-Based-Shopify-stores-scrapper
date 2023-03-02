3693797d844fea9361a013a8fcc157f785a2e2b7
Abdullah Anwar <abdullahan1928@gmail.com>
2023-03-02 12:28:30 +0500

    Remove duplicates & non-insta brands

--- new.py
+++ new.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 import calc_time as ct
 import excel_logic as ex
 import time
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 ct.start_time
 
@@ -16,12 +18,24 @@ dataframe1 = dataframe.active
 
 totalIndex = 1
 notAddedToExcel = 0
+instagramArray = []
+
+session = requests.Session()
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
+
 
 for row in range(1, dataframe1.max_row):
     for col in dataframe1.iter_cols(1, 1):
-        print(col[row].value)
-        page = requests.get(col[row].value)
-        soup = BeautifulSoup(page.content, 'html.parser')
+        try:
+            page = session.get(col[row].value)
+            soup = BeautifulSoup(page.content, 'html.parser')
+        except:
+            time.sleep(10)
+            continue
 
         try:
             brandName = soup.find(
@@ -39,12 +53,14 @@ for row in range(1, dataframe1.max_row):
                 instagram = 'None'
                 notAddedToExcel += 1
 
-        if instagram != 'None':
+        if instagram != 'None' and instagram not in instagramArray:
             ex.ws.cell(row=row+1, column=1).value = brandName
             ex.ws.cell(row=row+1, column=2).value = col[row].value
             ex.ws.cell(row=row+1, column=3).value = instagram
 
-        print(brandName, instagram)
+            instagramArray.append(instagram)
+
+            print(totalIndex, col[row].value, brandName, instagram)
 
         if totalIndex % 20 == 0:
             ex.wb.save(excel_file_to_be_saved)
@@ -52,6 +68,8 @@ for row in range(1, dataframe1.max_row):
             time.sleep(10)
 
         totalIndex += 1
+
+print("Not added to excel: ", notAddedToExcel)
 
 ex.wb.save(excel_file_to_be_saved)
 
